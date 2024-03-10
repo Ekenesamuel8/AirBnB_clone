d1dcf9ee33e18bf11bc21cf3883eb0dd19a5ecc2
ekene samuel <chikwenduekene@gmail.com>
2024-03-10 02:23:34 +0000

    second attempt on baSemodel

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,15 @@ class BaseModel:
         return f"[{self.__class__.__name__}] [{self.id}] {self.__dict__}"
 
     def save(self):
-        """Update the public instance attribute updated_at with the current datetime."""
+        """Update the public instance attribute
+           updated_at with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary containing all keys/values of __dict__ of the instance."""
+        """Return a dictionary containing all
+           keys/values of __dict__ of the instance.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
