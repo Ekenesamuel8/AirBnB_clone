70e60f0d9b6fa082d4340d7c206e2f208fe91885
ekene samuel <chikwenduekene@gmail.com>
2024-03-10 02:17:54 +0000

    second attempt on baSemodel

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,25 @@ import uuid
 from datetime import datetime
 
 
-class Base:
+class BaseModel:
     def __init__(self, **kwagrs):
-        self.__dict__ = kwagrs
-        self.id = str(uuid.uuid4().hex)
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = self.created_at
+        """Initialize BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-       # clasname = __class__.__name__
-        return f"<{__class__.__name__}> <{self.id}> {self.__dict__}"
+        """Return a string representation of the object."""
+        return f"[{self.__class__.__name__}] [{self.id}] {self.__dict__}"
 
-a = Base({"eke":"cow"})
-print(a)
+    def save(self):
+        """Update the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of __dict__ of the instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
