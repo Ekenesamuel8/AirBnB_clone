b08417ab098998cace8a7ba1f838a6d29f9f2eee
ekene samuel <chikwenduekene@gmail.com>
2024-03-12 23:51:38 +0000

    issue resolved

--- models/base_model.py
+++ models/base_model.py
@@ -13,6 +13,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
+        from models.engine.file_storage import FileStorage
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,12 +3,13 @@
 
 import json
 from models.base_model import BaseModel
-from models.user import User
+import os
+"""from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
-from models.review import Review
+from models.review import Review"""
 
 
 class FileStorage:
@@ -18,7 +19,7 @@ class FileStorage:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
-    __file_path = file.json
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -28,20 +29,20 @@ class FileStorage:
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         clobj = f"{self.__class__.__name__}.{obj.id}"
-        return FileStorage.__objects[clobj] = obj
+        FileStorage.__objects[clobj] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
         objdict = FileStorage.__objects
         object_dict = {obj: objdict[obj].to_dict() for obj in objdict.keys()}
         with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
-            json.dumps(object_dict, file)
+            json.dump(object_dict, file)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
+            with open(FileStorage.__file_path) as file:
+                objdict = json.load(file)
                 for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
