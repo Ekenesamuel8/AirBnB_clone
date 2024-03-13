720620e5f42c1024facf017a955d4764afbf5c48
ekene samuel <chikwenduekene@gmail.com>
2024-03-13 01:29:26 +0000

    console update

--- console.py
+++ console.py
@@ -11,6 +11,61 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program."""
         return True
 
+    def do_create(self, command):
+        """creates an object"""
+        if not command:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(command)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, command):
+        """Prints the string representation of an instance based on the class name and id."""
+        command = command.split()
+        if not command:
+            print("** class name missing **")
+            return
+        try:
+            class_name = command[0]
+            if len(command) < 2:
+                print("** instance id missing **")
+                return
+            instance_id = command[1]
+            key = "{}.{}".format(class_name, instance_id)
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, command):
+        """Deletes an instance based on the class name and id."""
+        command = command.split()
+        if not command:
+            print("** class name missing **")
+            return
+    
+        try:
+            class_name = args[0]
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
+
+
     def do_EOF(self, line):
         """EOF command to exit the program."""
         print() # Print a newline before exiting
